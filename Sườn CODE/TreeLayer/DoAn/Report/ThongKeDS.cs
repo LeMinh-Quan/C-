ddaeff05e195dbfabcb3ca293c483f304a062c07
LeMinh-Quan <aomachx@gmail.com>
2026-04-30 21:53:29 +0700

    Add source code

---
+++ Sườn CODE/TreeLayer/DoAn/Report/ThongKeDS.cs
@@ -0,0 +1,23 @@
+﻿namespace DoAn.Report
+{
+}
+
+namespace DoAn.Report
+{
+
+
+    public partial class DataSet1
+    {
+        partial class HoanThanhBHDataTable
+        {
+        }
+
+        partial class HDGuiNCCDataTable
+        {
+        }
+
+        partial class HDNhapHangDataTable
+        {
+        }
+    }
+}
